85ec1971d4d0519f49527aac3f0a2cecea0606b6
harry <harry@cell5.co.uk>
2019-09-23 09:15:25 +0100

    fix the seed script

--- seed-db.sh
+++ seed-db.sh
@@ -2,5 +2,5 @@
 echo SEEDING THE DB
 mysql --host=127.0.0.1 --port=3307 --user=esqimo --password=password  news < seed-db-with-sources
 echo LOADING THE ARTICLES...
-curl -X POST http://localhost:8000/load-news
+curl -X POST http://localhost:8000/news
 echo DONE!
